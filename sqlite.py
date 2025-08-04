44f9913683c020b7d3aed3a0e93a5aa197c22527
EvanDBush <ebush05@gmail.com>
2025-08-03 22:56:27 -0400

    removes deleted files

--- sqlite.py
+++
@@ -1,9 +0,0 @@
-import sqlite3
-import pandas as pd
-
-query = 'SELECT * FROM games LIMIT 10'
-
-with sqlite3.connect('./football.db') as football_db:
-    df = pd.read_sql(query, football_db)
-
-print(df)
