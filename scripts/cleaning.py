d8fac0be4bf7310dbd1820225290e94d08373907
EvanDBush <ebush05@gmail.com>
2025-08-06 13:50:24 -0400

    adds init file to help organize

--- scripts/cleaning.py
+++ scripts/cleaning.py
@@ -7,13 +7,27 @@ appearances_df = pd.read_csv('./data/raw/appearances.csv')
 appearances_df['date'] = pd.to_datetime(appearances_df['date'])
 # 2. drop 6 null values.
 appearances_df = appearances_df.dropna()
-appearances_df.to_csv('./data/clean/appearances.csv')
+appearances_df.to_csv('./data/clean/appearances.csv', index=False)
+
 # Club Games Table
+club_games_df = pd.read_csv('../data/raw/club_games.csv')
+# remove rows with missing ids
+# fill missing position values
+# fill missing managers names
+# drop remaining nulls 
+
 # Clubs Table
+
 # Competitions Table
+
 # Game Events Table
+
 # Game Line-ups Table
+
 # Games Table
+
 # Player Valuations Table
+
 # Players Table
+
 # Transfers Table
--- scripts/reports.py
+++ scripts/reports.py
@@ -9,17 +9,27 @@ def create_dataframe(table_name, folder):
     df = pd.read_csv(filepath)
     return df
 
-def eda_report(table_name, folder):
+def eda_info_describe(table_name, folder):
+    df= create_dataframe(table_name, folder)
+    column_info = df.info()
+    print(column_info)
+
+
+def eda_summary(table_name, folder):
     df = create_dataframe(table_name, folder)
+    
+    total_null_values = df.isnull().sum().sum() 
+    total_duplicates = df.duplicated().sum()
+
     print(
         f'''
     The {table_name} table has {df.shape[0]} rows and {df.shape[1]} columns.
     
-    There are {df.isnull().sum().sum()} null values. 
+    The column names are: \n{'\n'.join(df.columns)}
     
-    There are {df.duplicated().sum()} duplicates.
+    There are {total_null_values} null values. 
     
-    The column names are: \n{', '.join(df.columns)} 
+    There are {total_duplicates} duplicates.
         ''')
 
 def numeric_columns_report(table_name, folder):
@@ -32,7 +42,7 @@ def numeric_columns_report(table_name, folder):
     
 def get_all_eda_reports(table_list, folder):
     for table in table_list:
-        eda_report(table, folder )
+        eda_summary(table, folder )
 
 def get_all_numeric_reports(table_list, folder):
     total_report_df = pd.DataFrame()
@@ -43,10 +53,11 @@ def get_all_numeric_reports(table_list, folder):
     
     print(total_report_df)
 
-eda_report('club_games', 'raw')
-numeric_columns_report('club_games', 'raw')
+#eda_summary('club_games', 'raw')
+#numeric_columns_report('club_games', 'raw')
 #get_all_eda_reports(table_list, 'raw')
 # numeric_columns_report('players', 'raw')
 # numeric_columns_report('clubs', 'raw')
 # numeric_columns_report('player_valuations', 'raw')
 #get_all_numeric_reports(table_list, 'raw')
+eda_info_describe('games', 'raw')
