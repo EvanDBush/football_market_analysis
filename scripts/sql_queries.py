198a9ab872f880494a49de6b7816a2d9c1020d9f
EvanDBush <ebush05@gmail.com>
2025-08-09 00:58:33 -0400

    working on sql queries

--- scripts/sql_queries.py
+++ scripts/sql_queries.py
@@ -1,14 +1,28 @@
 import sqlite3
 import pandas as pd
 
-query = 'SELECT * FROM games LIMIT 10'
+test_query = 'SELECT * FROM games LIMIT 10'
 
 # Joined table queries
-
-
+club_value_query = """
+SELECT 
+    clubs.club_id, 
+    clubs.name,
+    SUM(player_valuations.market_value_in_eur) AS club_market_value
+FROM
+    player_valuations
+JOIN
+    clubs ON player_valuations.current_club_id = clubs.club_id
+GROUP BY
+    clubs.name
+ORDER BY 
+    club_market_value DESC;
+"""
 
 def query_db(query):
 
     with sqlite3.connect('./football.db') as football_db:
         df = pd.read_sql(query, football_db)
     print(df)
+
+query_db(club_value_query)
